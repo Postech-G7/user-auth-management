1057fe7579df9da06ea32dde172b5ac9571ec9e4
charlis.monhol <charlis.monhol@gmail.com>
2025-02-09 19:33:04 -0300

    correção na clase user-model

--- src/users/infraestructure/database/prisma/models/user-model.mapper.ts
+++ src/users/infraestructure/database/prisma/models/user-model.mapper.ts
@@ -1,63 +1,20 @@
-import { UserModelMapper } from '../models/user-model.mapper';
-import { UserEntity } from '../../../../domain/entities/user.entity';
 import { ValidationError } from '../../../../../shared/domain/errors/validation-error';
+import { UserEntity } from '../../../../domain/entities/user.entity';
+import { User } from '@prisma/client';
 
-describe('UserModelMapper', () => {
-  describe('toEntity', () => {
-    it('should map a valid Prisma model to a UserEntity', () => {
-      const mockPrismaModel = {
-        id: 1,
-        name: 'John Doe',
-        email: 'john.doe@example.com',
-        password: 'hashed-password',
-        createdAt: new Date(),
-      };
-
-      const entity = UserModelMapper.toEntity(mockPrismaModel);
-
-      expect(entity).toBeInstanceOf(UserEntity);
-      expect(entity.id).toBe('1');
-      expect(entity.name).toBe('John Doe');
-      expect(entity.email).toBe('john.doe@example.com');
-      expect(entity.password).toBe('hashed-password');
-      expect(entity.createdAt).toEqual(mockPrismaModel.createdAt);
-    });
-
-    it('should throw ValidationError if the entity creation fails', () => {
-      const invalidPrismaModel = {
-        id: 1,
-        name: '', // Invalid name (empty string)
-        email: 'invalid-email',
-        password: 'hashed-password',
-        createdAt: new Date(),
-      };
-
-      expect(() => UserModelMapper.toEntity(invalidPrismaModel)).toThrow(
-        ValidationError
-      );
-    });
-  });
-
-  describe('toPrisma', () => {
-    it('should map a UserEntity to a Prisma model', () => {
-      const mockEntity = new UserEntity(
-        {
-          name: 'Jane Doe',
-          email: 'jane.doe@example.com',
-          password: 'hashed-password',
-          createdAt: new Date(),
-        },
-        '456'
-      );
-
-      const prismaModel = UserModelMapper.toPrisma(mockEntity);
-
-      expect(prismaModel).toEqual({
-        name: 'Jane Doe',
-        email: 'jane.doe@example.com',
-        password: 'hashed-password',
-        createdAt: mockEntity.createdAt,
-      });
-    });
-  });
-});
+export class UserModelMapper {
+  static toEntity(model: User) {
+    const { name, email, password, createdAt } = model;
+    const entity = {
+      name,
+      email,
+      password,
+      createdAt,
+    };
+    try {
+      return new UserEntity(entity, model.id);
+    } catch (error) {
+      throw new ValidationError('Entity not loaded');
+    }
+  }
+}
